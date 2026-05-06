58996fde049fc83b12899d9ae90a9447a7f5d6f2
hcrtlr <139785601+hcrtlr@users.noreply.github.com>
2026-05-06 14:31:09 +0300

    fix useSearchParams prerender issue

--- src/app/customer/checkout/page.tsx
+++ src/app/customer/checkout/page.tsx
@@ -1,5 +1,5 @@
 ﻿"use client"
-
+export const dynamic = "force-dynamic"
 import type React from "react"
 import { useEffect, useState, useCallback } from "react"
 import { useRouter } from "next/navigation"
